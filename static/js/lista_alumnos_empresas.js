64226be81ff095b5e433df43f6685dbf2d400a0e
AleeCF <94109004+AleeCF@users.noreply.github.com>
2022-11-30 17:25:14 +0100

    Add files via upload
    Cambio en la tabla de Js.

--- static/js/lista_alumnos_empresas.js
+++ static/js/lista_alumnos_empresas.js
@@ -1,6 +1,6 @@
 //FUNCION PARA AGREGAR FILAS
 const agregarFila = () => {
-    document.getElementById('tablaprueba').insertRow(-1).innerHTML = '<td><input type="text" size="10"></td><td><input type="text" size="10"></td><td><input type="text" size="11"></td><td><input type="text" size="10"></td><td><input class="correo" type="text" size="10"></td><td><button type="button" class="ver">Ficha</button></td><td><button type="button" class="ver">Borrar</button></td><td></td>'
+    document.getElementById('tabla_alumnos').insertRow(-1).innerHTML = '<td><input type="text" size="10"></td><td><input type="text" size="10"></td><td><input type="text" size="11"></td><td><input type="text" size="10"></td><td><input class="correo" type="text" size="10"></td><td><button type="button" class="ver">Ficha</button></td><td><button type="button" class="ver">Borrar</button></td><td></td>'
   }
   
   
